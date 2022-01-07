062aef3c104cd5f8458e1cc7799ac80ca83149c1
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2022-01-07 12:09:03 +0330

    Complete Logout Feature

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logout } from "../redux/slices/user";
 
 const Logout = () => {
-  return <div>Logout Page</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    localStorage.removeItem("token");
+    dispatch(logout());
+    console.log("logout com");
+    navigate("/");
+    // eslint-disable-next-line
+  }, []);
+  return null;
 };
 
 export default Logout;
